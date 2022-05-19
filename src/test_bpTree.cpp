20a67c14c391acd7ca1242f31a4dfdcdf5204324
Vexoben <37978582+Vexoben@users.noreply.github.com>
2022-05-19 19:11:10 +0800

    bpTree debug

--- src/test_bpTree.cpp
+++ src/test_bpTree.cpp
@@ -2,8 +2,12 @@
 #include "bpTree.hpp"
 using namespace std;
 
+struct point {
+    int x, y;
+};
+
 int main() {
-    Bptree<int, int, std::less<int>, 5, 5> tr("Bptree_test");
+    Bptree<int, int, std::less<int>, std::equal_to<int>, 5, 5> tr("Bptree_test");
     tr.insert(1, 1);
     tr.insert(2, 2);
     tr.insert(3, 3);
@@ -15,5 +19,13 @@ int main() {
     cout << ans << endl;
     cout << tr.find(2, ans) << endl;
     cout << ans << endl;
+    tr.modify(5, 100);
+    cout << tr.find(5, ans) << endl;
+    cout << ans << endl;
+    vector<pair<int, int>> vec;
+    tr.range_search(2, 6, vec);
+    for (int i = 0; i < vec.size(); ++i) {
+        cout << vec[i].first << ' ' << vec[i].second << endl;
+    }
     return 0;
 }
